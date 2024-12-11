c45579b0d2573f141beb52360847ecde8bbce00f
Shiva Kumar Karru <103453725+Shivaksk119@users.noreply.github.com>
2024-12-12 01:55:09 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,8 +1,8 @@
 //your JS code here. If required.
 function createPromise(){
 	return new Promise((resolve)=>{
-		let time = Math.random()*2000 + 1000
-		setTimeout(resolve(time),time)
+		let time = Math.random()*2 + 1
+		setTimeout(resolve(time),time*1000)
 	})
 }
 
@@ -12,15 +12,20 @@ let promise3 = createPromise();
 
 let tableBody = document.getElementById('output')
 
-Promise.all([promise1,promise2,promise3]).then((times)=>{
+let row1 = tableBody.insertRow()
+let cell = row1.insertCell()
+cell.colSpan = 2
+cell.innerHTML = 'Loading...'
 
+Promise.all([promise1,promise2,promise3]).then((times)=>{
+	tableBody.deleteRow(0)
 	for(let i=0;i<3;i++){
 		let row = tableBody.insertRow(i);
 		let cell1 = row.insertCell(0);
 		let cell2 = row.insertCell(1);
 
 		cell1.innerHTML = `Promise ${i+1}`
-		cell2.innerHTML = (times[i]/1000).toFixed(3)
+		cell2.innerHTML = times[i]
 	}
 
 	//fourth row
@@ -28,5 +33,5 @@ Promise.all([promise1,promise2,promise3]).then((times)=>{
 	let cell1 = lastrow.insertCell(0);
 	let cell2 = lastrow.insertCell(1);
 	cell1.innerHTML = `Total`
-	cell2.innerHTML = ((times[0]+times[1]+times+[2])/1000).toFixed(3)
+	cell2.innerHTML = ((times[0]+times[1]+times+[2])).toFixed(3)
 })
