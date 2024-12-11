05444bc5ba8b7e6ac47c473c9e2aa6769fe0363c
Shiva Kumar Karru <103453725+Shivaksk119@users.noreply.github.com>
2024-12-12 01:05:10 +0530

    created a submission

--- script.js
+++ script.js
@@ -14,13 +14,20 @@ let tableBody = document.getElementById('output')
 
 Promise.all([promise1,promise2,promise3]).then((times)=>{
 
-	for(let i=0;i<2;i++){
+	for(let i=0;i<3;i++){
 		let row = tableBody.inserRow(i);
 
 		let cell1 = row.insertCell(0);
 		let cell2 = row.insertCell(1);
 
-		cell1.innerText = `Promise ${i+1}`
-		cell2.innerText = (times[i]/1000).toFixed(3)
+		cell1.innerHTML = `Promise ${i+1}`
+		cell2.innerHTML = (times[i]/1000).toFixed(3)
 	}
+
+	//fourth row
+	let lastrow = tableBody.insertRow(3)
+	let cell1 = row.insertCell(0);
+	let cell2 = row.insertCell(1);
+	cell1.innerHTML = `Total`
+	cell2.innerHTML = ((times[0]+times[1]+times+[2])/1000).toFixed(3)
 })
