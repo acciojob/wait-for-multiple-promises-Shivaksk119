9cbcdd788de1acdfdf219cf03f67229123159519
Shiva Kumar Karru <103453725+Shivaksk119@users.noreply.github.com>
2024-12-19 22:51:57 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,7 +1,7 @@
 //your JS code here. If required.
 function createPromise(){
 	return new Promise((resolve)=>{
-		let time = Math.random()*2 + 1
+		let time = Math.random()*3 + 1
 		setTimeout(resolve(time),time*1000)
 	})
 }
@@ -21,7 +21,7 @@ Promise.all([promise1,promise2,promise3]).then((times)=>{
 		let cell2 = row.insertCell(1);
 
 		cell1.innerHTML = `Promise ${i+1}`
-		cell2.innerHTML = times[i]
+		cell2.innerHTML = Math.ceil(times[i])
 	}
 
 	//fourth row
